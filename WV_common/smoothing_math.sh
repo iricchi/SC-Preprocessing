71d589af1c67d91c2fb9da2a0f6bc30b5a562a99
iricchi <ilaria.ricchi@epfl.ch>
2022-07-27 16:59:46 +0200

    [FIX] Bug fixed on smoothing_math

--- WV_common/smoothing_math.sh
+++ WV_common/smoothing_math.sh
@@ -8,72 +8,11 @@ cd Processing
 
 mkdir -p Smoothing
 cd Smoothing
+cp ../../mfmri_denoised_n.nii.gz .
 
-# First, isolate slice init 
-if [ ! -f slice_init.nii.gz ]; then
-        fslroi ../../mfmri_denoised_n slice_init 0 -1 0 -1 0 1 0 -1
-fi
-
-dim=$(fslsize ../../mfmri_denoised_n.nii.gz)
-arrdim=($dim)
-z=${arrdim[5]}
-maxz=$(( z-1 ))
-
-# Then, isolate slice last
-if [ ! -f slice_last.nii.gz ]; then
-        fslroi ../../mfmri_denoised_n slice_last 0 -1 0 -1 $maxz 1
-fi
-
-
-# And for mask as well
-if [ ! -f ../../Segmentation/mask_init.nii.gz ]; then
-        fslroi ../../Segmentation/mask_sc.nii.gz ../../Segmentation/mask_init 0 -1 0 -1 0 1 0 -1
-fi
-
-if [ ! -f ../../Segmentation/mask_last.nii.gz ]; then
-        fslroi ../../Segmentation/mask_sc.nii.gz ../../Segmentation/mask_last 0 -1 0 -1 $maxz 1
-fi
-
-#rm residuals_native_padded.nii.gz
-
-if [ ! -f mfmri_denoised_padded.nii.gz ]; then
-        fslmerge -z mfmri_denoised_padded ../../mfmri_denoised_n slice_last slice_last slice_last slice_last slice_last
-        fslmerge -z mfmri_denoised_padded slice_init slice_init slice_init slice_init slice_init mfmri_denoised_padded
-fi
-
-if [ ! -f mask_padded.nii.gz ]; then
-        fslmerge -z mask_padded ../../Segmentation/mask_sc ../../Segmentation/mask_last ../../Segmentation/mask_last ../../Segmentation/mask_last ../../Segmentation/mask_last ../../Segmentation/mask_last
-        fslmerge -z mask_padded ../../Segmentation/mask_init ../../Segmentation/mask_init ../../Segmentation/mask_init ../../Segmentation/mask_init ../../Segmentation/mask_init mask_padded
-fi
-
-# Create temporary folder
-rm -r tmp
-mkdir -p tmp
-
-# Copy input data to tmp folder & split
-cp mfmri_denoised_padded.nii.gz tmp
-cd tmp
-sct_image -i mfmri_denoised_padded.nii.gz -split t -o mfmri_denoised_padded.nii.gz
-rm mfmri_denoised_padded.nii.gz
-
-files_to_smooth="$PWD"/*
-for b in $files_to_smooth; do # Loop through all files
-        tmp_name=${b%.*}  # remove extensions
-        name=${tmp_name%.*}
-        sct_maths -i $b -o $name"_smooth.nii.gz" -smooth 0.85,0.85,2.55
-        rm $b
-done
-
-# Concat, put back in initial folder and delete temporary folder
-fslmerge -t sn_mfmri_denoised_padded_2x2x6 mfmri_denoised_padded*_smooth.nii.gz
-mv sn_mfmri_denoised_padded_2x2x6.nii.gz ..
-cd ..
-
-sct_crop_image -i sn_mfmri_denoised_padded_2x2x6.nii.gz -zmin 5 -zmax -6 -o sn_mfmri_denoised_2x2x6.nii.gz
+sct_maths -i mfmri_denoised_n.nii.gz -o sn_mfmri_denoised_2x2x6.nii.gz -smooth 0.85,0.85,2.55
 mv sn_mfmri_denoised_2x2x6.nii.gz ../../sn_mfmri_denoised_2x2x6.nii.gz
-
 cd ../..
-
 # Copy header info
 fslcpgeom mfmri_denoised.nii.gz sn_mfmri_denoised_2x2x6.nii.gz
 
